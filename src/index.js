fbbe6057eb2928647a1bff99b616b747db89536c
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-02-09 15:22:16 -0800

    commit tree c5ec2f0ccfef13adff864a07143850c0f787cd28

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//
+import React from "react";
+import ReactDOM from "react-dom";
+// Step 1. Import react-router functions
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+
+const link = {
+  width: "100px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+const Navbar = () => (
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: "darkblue",
+      }}
+    >
+      Home
+    </NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: "darkblue",
+      }}
+    >
+      About
+    </NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: "darkblue",
+      }}
+    >
+      Login
+    </NavLink>
+  </div>
+);
 
 const Home = () => {
   return (
@@ -8,8 +58,41 @@ const Home = () => {
     </div>
   );
 };
+const About = () => {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+};
 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+};
+
+// Step 2. Changed to have router coordinate what is displayed
 ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+    </div>
+  </Router>,
+  document.getElementById("root")
 );
